54cbad6591017fbc77c408a9c83bcf230051964d
enzoimola <ennzoimola@hotmail.com>
2023-01-16 15:53:31 -0300

    [WIP] - Directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,69 @@
         <div class="w3-border">
             <div class="w3-grey" style="height:24px;width:50%"></div>
         </div>
+
+        <div class="card-container">
+            <div class="card-header">
+                <div class="card-header-container">
+                        <label class="card-header-name">Luis Navarro</label>
+                        <div class="vertically-separator"></div>
+                        <label class="card-header-profession">Surgeon - General</label>
+                </div>
+                <div class="card-header-description">
+                    <span>Sponsored</span>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="card-body-picture"></div>
+                <div class="card-body-stadistic">
+                    <div>
+                        <a>ProfilePoints 80/80</a>
+                    </div>
+                    <div>
+                        <a>Avg. RatingL 5/5</a>
+                    </div>
+                    <div>
+                        <div class="clip-star"></div>
+                        <div class="clip-star"></div>
+                        <div class="clip-star"></div>
+                        <div class="clip-star"></div>
+                        <div class="clip-star"></div>
+                    </div>
+                </div>
+
+                <div class="card-body-location">
+                    <div class="card-body-description-title">
+                        <label>MAIN LOCATION</label>
+                    </div>
+                    <div class="card-body-description-subtitle">
+                        <label>327 East 65th Street
+                            New York, New York 10065</label>
+                    </div>
+                </div>
+
+                <div class="card-body-highlights">
+                    <div class="card-body-description-title">
+                        <label>HIGHLIGHTS</label>
+                    </div>
+                    <div class="card-body-description-subtitle">
+                        <li>Board Certified</li>
+                        <li>Has Online Scheduling</li>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer">
+                <div>
+                    <label>(908) 897-0737</label>
+                </div>
+                <div>
+                    <button>Request appointment</button>
+                    <button>View Profile</button>
+                </div>
+
+            </div>
+
+        </div>
     </div>
 </x-layouts.app>
