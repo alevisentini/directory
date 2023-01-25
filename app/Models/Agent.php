fd7fd2a037192de0bb01c2c9e69eeee32d994277
mferrer80 <miriamferrer@gmail.com>
2023-01-25 13:48:44 -0300

    Defining relationship

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -13,4 +13,9 @@ class Agent extends Model
     {
         return $this->belongsTo(Business::class);
     }
+
+    public function address()
+    {
+        return $this->hasMany(Address::class);
+    }
 }
