86c2b395c0d5a8e9cb9f50bf4a1dd148cffc4f9b
Pablo Picapietra <ppicapietra@gmail.com>
2023-01-25 09:12:37 -0300

    Initial section template

---
+++ resources/views/components/details-box.blade.php
@@ -0,0 +1,16 @@
+<div class="details-box">
+
+</div>
+
+<style>
+    .details-box {
+        background-color: #fff;
+        margin-top: 15px;
+        padding: 15px;
+        -webkit-border-radius: 2px;
+        -moz-border-radius: 2px;
+        -ms-border-radius: 2px;
+        -o-border-radius: 2px;
+        border-radius: 2px;
+    }
+</style>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,37 +1,53 @@
 <x-layouts.app title="Profile" meta-description="Profile">
 
     <div class="breadcrumb-container">
-        <div >
+        <div>
             <label></label>
             <ol class="breadcrumb">
                 <div>
                     <i class="fa fa-home"></i>
                     <a href="/">HOME</a>
                 </div>
-                @for($i = 2; $i <= count(Request::segments()); $i++)
-                    <div>
-                        <a href="{{ URL::to( implode( '/', array_slice(Request::segments(), 0 ,$i, true)))}}">
-                            {{strtoupper(Request::segment($i))}}
-                        </a>
-                    </div>
-                @endfor
-            </ol>
+                @for($i = 2; $i <= count(Request::segments()); $i++) <div>
+                    <a href="{{ URL::to( implode( '/', array_slice(Request::segments(), 0 ,$i, true)))}}">
+                        {{strtoupper(Request::segment($i))}}
+                    </a>
         </div>
-        <div>
-            <button onclick="window.location='{{ url('/') }}'">Back to Results</button>
-        </div>
+        @endfor
+        </ol>
+    </div>
+    <div>
+        <button onclick="window.location='{{ url('/') }}'">Back to Results</button>
+    </div>
     </div>
 
-    <div class="container-page">
-        <div>
-
-
-
-
+    <main>
+        <div id="profile-header">
+            <div id="profile-picture">
+                <img src="https://via.placeholder.com/150" />
+            </div>
+            <div id="profile-title">
+                <h2>[Profile Name, MD.]</h2>
+                <h4>[Profile roles]</h4>
+            </div>
         </div>
-
-
-    </div>
+        <div id="profile-middle-content" class="content-in-cols">
+            <div id="profile-contact-info">
+                <img src="https://via.placeholder.com/200x400?text=Contact+Menu" />
+            </div>
+            <div id="profile-bio">
+                <div id="profile-experiences">
+                <img src="https://via.placeholder.com/800x200?text=Box-details" />
+                </div>
+                <div id="profile-education">
+                <img src="https://via.placeholder.com/800x200?text=Box-details" />
+                </div>
+                <div id="profile-locations">
+                <img src="https://via.placeholder.com/800x200?text=Map+location" />
+                </div>
+            </div>
+        </div>
+    </main>
 
 </x-layouts.app>
 
@@ -58,5 +74,60 @@
 
     .breadcrumb div:last-child:after {
         content: none;
+    }
+
+
+    main {
+        display: flex;
+        flex-flow: column wrap;
+    }
+
+    /* testing only */
+
+    #profile-header {
+        display: flex;
+        flex-flow: row wrap;
+        background-color: #e1eae6;
+        margin-bottom: 20px;
+    }
+
+    #profile-picture {
+        height: 150px;
+        width: 150px;
+        background-color: #007cb8;
+    }
+
+    #profile-title {
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: start;
+        margin-left: 20px;
+    }
+
+    #profile-title h2 {
+        font-weight: 400;
+        margin-bottom: 10px;
+        font-size: 30px;
+        font-size: 3rem;
+        margin: 0;
+    }
+
+    #profile-title h4 {
+        font-size: 14px;
+        font-size: 1.3rem;
+        font-weight: 300;
+        margin: 0;
+    }
+
+    .content-in-cols {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 20px;
+    }
+
+    #profile-bio {
+        display: flex;
+        flex-flow: column wrap;
+        gap: 20px;
     }
 </style>
